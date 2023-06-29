a8aa93e8f2e9ba77729beb6f91478cde497d83fc
RuanGemmer <ruangemmer@live.com>
2023-06-29 16:51:11 -0300

    Realizado configurações de logout

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -8,10 +10,18 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.router.navigate(['home'])
+  }
+
+  logout(){
+    this.router.navigate(['login']);
+    this.authService.logout();
+    this.toastr.info('Logout realizado com sucesso!', 'Logout', {timeOut: 7000})
   }
 
 }
