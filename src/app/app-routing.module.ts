e63c56c1d966ec61908deaa4adf81b96d7549727
RuanGemmer <ruangemmer@live.com>
2023-06-29 17:39:13 -0300

    criado componente tecnico-create e adicionado rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [
   {
@@ -14,7 +15,9 @@ const routes: Routes = [
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       {path: 'home', component: HomeComponent},
-      {path: 'tecnicos', component: TecnicoListComponent}
+
+      {path: 'tecnicos', component: TecnicoListComponent},
+      {path: 'tecnicos/create', component: TecnicoCreateComponent}
     ]
   }
 ];
