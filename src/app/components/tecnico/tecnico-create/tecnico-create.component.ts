a9e0fecb1080cadc76ba8468fc5c8a6aaabc006f
RuanGemmer <ruangemmer@live.com>
2023-06-30 11:28:04 -0300

    Adicionado mascaras e validações no Tecnico-Create

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./tecnico-create.component.css']
 })
 export class TecnicoCreateComponent {
+
+  nome: FormControl = new FormControl(null,
+    [Validators.required,
+    Validators.minLength(3)]);
+
+  cpf: FormControl = new FormControl(null,
+    [Validators.required,
+    Validators.minLength(11)]);
+
+  email: FormControl = new FormControl(null,
+    Validators.email);
+
+  senha: FormControl = new FormControl(null,
+    Validators.minLength(3));
+
+  validaCampos(): boolean {
+    return this.nome.valid
+      && this.senha.valid
+      && this.email.valid
+      && this.cpf.valid;
+  }
 
 }
