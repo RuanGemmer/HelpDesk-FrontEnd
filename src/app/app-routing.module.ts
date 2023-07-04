b67e074cf8cb75fb4e742c97adf75e31548dacaf
RuanGemmer <ruangemmer@live.com>
2023-07-04 14:16:19 -0300

    Criado método chamado-update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
 
       { path: 'chamados', component: ChamadoListComponent },
       { path: 'chamados/create', component: ChamadoCreateComponent },
-      { path: 'chamados/update/id', component: ChamadoUpdateComponent },
+      { path: 'chamados/update/:id', component: ChamadoUpdateComponent },
     ]
   }
 ];
--- src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ChamadoService } from 'src/app/services/chamado.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-chamado-update',
@@ -6,5 +16,121 @@ import { Component } from '@angular/core';
   styleUrls: ['./chamado-update.component.css']
 })
 export class ChamadoUpdateComponent {
+  chamado: Chamado = {
+    prioridade: '',
+    status: '',
+    titulo: '',
+    observacoes: '',
+    tecnico: '',
+    cliente: '',
+    nomeCliente: '',
+    nomeTecnico: ''
+  }
+
+  clientes: Cliente[] = [];
+  tecnicos: Tecnico[] = [];
+  clientesFiltrados: Cliente[] = [];
+  tecnicosFiltrados: Tecnico[] = [];
+  filtro: string = '';
+
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status: FormControl = new FormControl(null, [Validators.required]);
+  titulo: FormControl = new FormControl(null, [Validators.required, Validators.minLength(3)]);
+  descricao: FormControl = new FormControl(null, [Validators.required, Validators.minLength(3)]);
+  tecnico: FormControl = new FormControl(null, [Validators.required]);
+  cliente: FormControl = new FormControl(null, [Validators.required]);
+
+  constructor(
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private chamadoService: ChamadoService,
+    private toastr: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+    this.findAllClientes();
+    this.findAllTecnicos();
+  }
+
+  findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(resposta => {
+      this.chamado = resposta;
+    }, ex => {
+      this.toastr.error(ex.error.error);
+    })
+  }
+
+  update(): void {
+    this.chamadoService.update(this.chamado).subscribe(resposta => {
+      this.toastr.success('Chamado atualizado com sucesso', 'Chamado Atualizado');
+      this.router.navigate(['chamados'])
+
+    }, ex => {
+      this.toastr.error(ex.error.error);
+    })
+  }
+
+  findAllClientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    })
+  }
+
+  applyFilterCliente(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.filtro = filterValue;
+
+    this.clientesFiltrados = this.clientes.filter(cli =>
+      cli.nome.toLowerCase().includes(filterValue)
+    );
+  }
+
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta;
+    })
+  }
+
+  applyFilterTecnico(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.filtro = filterValue;
+
+    this.tecnicosFiltrados = this.tecnicos.filter(tec =>
+      tec.nome.toLowerCase().includes(filterValue)
+    );
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid &&
+      this.status.valid &&
+      this.titulo.valid &&
+      this.descricao.valid &&
+      this.tecnico.valid &&
+      this.cliente.valid
+  }
+
+  retornaStatus(status: any): string {
+    if (status == '0') {
+      return 'ABERTO'
+    } else if (status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == '0') {
+      return 'BAIXA'
+    } else if (prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
 
 }
