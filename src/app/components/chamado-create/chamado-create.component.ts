8a6bf7843456b22424d735e699679469b8f0d949
RuanGemmer <ruangemmer@live.com>
2023-07-04 13:19:48 -0300

    Implementado serviço para criar novo Chamado

--- src/app/components/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado-create/chamado-create.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ChamadoService } from 'src/app/services/chamado.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-chamado-create',
@@ -8,12 +17,82 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ChamadoCreateComponent {
 
+  chamado: Chamado = {
+    prioridade: '',
+    status: '',
+    titulo: '',
+    observacoes: '',
+    tecnico: '',
+    cliente: '',
+    nomeCliente: '',
+    nomeTecnico: ''
+  }
+
+  clientes: Cliente[] = [];
+  tecnicos: Tecnico[] = [];
+  clientesFiltrados: Cliente[] = [];
+  tecnicosFiltrados: Tecnico[] = [];
+  filtro: string = '';
+
   prioridade: FormControl = new FormControl(null, [Validators.required]);
   status: FormControl = new FormControl(null, [Validators.required]);
   titulo: FormControl = new FormControl(null, [Validators.required, Validators.minLength(3)]);
   descricao: FormControl = new FormControl(null, [Validators.required, Validators.minLength(3)]);
   tecnico: FormControl = new FormControl(null, [Validators.required]);
   cliente: FormControl = new FormControl(null, [Validators.required]);
+
+  constructor(
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private chamadoService: ChamadoService,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.findAllClientes();
+    this.findAllTecnicos();
+  }
+
+  create(): void {
+    this.chamadoService.create(this.chamado).subscribe(resposta => {
+      this.toastr.success('Chamado criado com sucesso', 'Novo Chamado');
+      this.router.navigate(['chamados'])
+
+    }, ex => {
+      this.toastr.error(ex.error.error);
+    })
+  }
+
+  findAllClientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    })
+  }
+
+  applyFilterCliente(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.filtro = filterValue;
+
+    this.clientesFiltrados = this.clientes.filter(cli =>
+      cli.nome.toLowerCase().includes(filterValue)
+    );
+  }
+
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta;
+    })
+  }
+
+  applyFilterTecnico(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.filtro = filterValue;
+
+    this.tecnicosFiltrados = this.tecnicos.filter(tec =>
+      tec.nome.toLowerCase().includes(filterValue)
+    );
+  }
 
   validaCampos(): boolean {
     return this.prioridade.valid &&
